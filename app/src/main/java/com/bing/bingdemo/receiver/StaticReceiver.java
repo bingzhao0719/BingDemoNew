66944bc837c543872846cb6b0db80eb46dba1954
wubz <98078787>
2020-08-30 22:12:41 +0800

    广播

---
+++ app/src/main/java/com/bing/bingdemo/receiver/StaticReceiver.java
@@ -0,0 +1,12 @@
+package com.bing.bingdemo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StaticReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
