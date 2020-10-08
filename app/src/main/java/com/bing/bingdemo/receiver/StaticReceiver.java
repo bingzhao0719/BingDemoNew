bed2494da5c3bcd081392a80752c2a11728b55ca
wubz <wubingzhao@richinfo.cn>
2020-10-08 23:13:11 +0800

    更改gradle版本

--- app/src/main/java/com/bing/bingdemo/receiver/StaticReceiver.java
+++ app/src/main/java/com/bing/bingdemo/receiver/StaticReceiver.java
@@ -3,10 +3,11 @@ package com.bing.bingdemo.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class StaticReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
-
+    public synchronized void onReceive(Context context, Intent intent) {
+        Log.i("wubingzhao", "onReceive 我接收到静态广播拉: ");
     }
 }
